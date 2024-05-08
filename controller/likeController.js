61916a556145e4168b6c5919b3f47dc7bedb6715
Sahiljain24 <24sahilj@gmail.com>
2024-05-08 23:29:32 +0530

    update

--- controller/likeController.js
+++ controller/likeController.js
@@ -1,16 +1,16 @@
 const Like = require("../models/liseController");
 const Post = require("../models/postController")
 
-exports.likeController = async(res,res)=>{
+exports.likeController = async(req,res)=>{
     try{
       const {post,user}= req.body;
        const like = new Like({
         post,user
        });
        const saveLike = await like.save();
-       const updatePost = await Post.findByIdAndUpdate(post,{$push:{likes:lsaveLike._id}},{new:true}).populate("likes").exec();
+       const updatePost = await Post.findByIdAndUpdate(post,{$push:{likes:saveLike._id}},{new:true}).populate("likes").exec();
        res.json({
-        
+        post:updatePost,
        })
     }
     catch(err){
@@ -19,4 +19,21 @@ exports.likeController = async(res,res)=>{
             error:"Not able to Like"
          })
     }
-}
+};
+
+exports.unLikeController = async(req,res)=>{
+    try{
+      const {post,like}= req.body;
+      const deleteLike = await Like.findByIdAndDelete({post:post,_id:like})
+       const updatePost = await Post.findByIdAndUpdate(post,{$pull:{likes:like}},{new:true}).populate("likes").exec();
+       res.json({
+        post:updatePost,
+       })
+    }
+    catch(err){
+         console.log(err);
+         res.status(500).json({
+            error:"Not able to UnLike"
+         })
+    }
+};
--- routes/blogs.js
+++ routes/blogs.js
@@ -5,9 +5,12 @@ const router = express.Router();
 
 const{commentController} =require("../controller/commentController");
 const { postController, getAllPost } = require("../controller/postController");
+const { likeController, unLikeController } = require("../controller/likeController");
 
 router.post("/comment/create",commentController);
 router.post("/post/create",postController)
 router.get("/posts",getAllPost)
- 
+router.get("/likes/like",likeController)
+router.delete("/likes/unlike",unLikeController)
+
 module.exports =router;
